9a1ac78a63f880ecd2484fdf8339e3561a051074
Valerie Y Sarge <vysarge@mit.edu>
2015-11-01 16:48:59 -0500

    testing

---
+++ vga.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+
+
+module vga(input vga_clock,
+            output reg [9:0] hcount = 0,    // pixel number on current line
+            output reg [9:0] vcount = 0,	 // line number
+            output vsync, hsync, at_display_area);
+    // Counters.
+    always @(posedge vga_clock) begin
+        if (hcount == 799) begin
+            hcount <= 0;
+        end
+        else begin
+            hcount <= hcount +  1;
+        end
+        if (vcount == 524) begin
+            vcount <= 0;
+        end
+        else if(hcount == 799) begin
+            vcount <= vcount + 1;
+        end
+    end
+    
+    assign hsync = (hcount < 96);
+    assign vsync = (vcount < 2);
+    assign at_display_area = (hcount >= 144 && hcount < 784 && vcount >= 35 && vcount < 515);
+endmodule
