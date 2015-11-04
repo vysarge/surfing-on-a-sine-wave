d094ff75383d443e08f68f58cb4cdf4530523daa
Valerie Y Sarge <vysarge@mit.edu>
2015-11-04 18:21:53 -0500

    display proof of concept (sans sprite ROMs)

--- display.v
+++ display.v
@@ -1,10 +1,20 @@
 `timescale 1ns / 1ps
 
+/////////////////////////////////////////////////////////////////
+// 
+// Display module; takes in data on player offset, player position,
+// and waveform, as well as xvga signals.
+// 
+// wave_we is active high and enables wave_prof data to be written
+// at wave_index; this is clocked by posedge vclock
+// 
+/////////////////////////////////////////////////////////////////
 module display(input reset,
                input [10:0] p_offset, //horizontal offset
                input [9:0] p_vpos, //vertical position of character
                input [9:0] wave_prof, //waveform profile
-               input wave_clk, //clocks in waveform data
+               input [10:0] wave_index,
+               input wave_we, //write enable; wave data clocked by vclock
                input vclock, //65 mhz
                input [10:0] hcount, //0 at left
                input [9:0] vcount, //0 at top
@@ -18,35 +28,75 @@ module display(input reset,
     reg [10:0] offset;
     reg [9:0] vpos;
     reg [9:0] wave[1023:0];
+    reg [9:0] next_wave[1023:0];
+    reg [10:0] char_x;
+    reg char_frame;
     
     //sprite pixel outputs
     wire [11:0] character_rgb;
+    wire [11:0] l_bg_rgb;
+    wire [11:0] u_bg_rgb;
     
     //sprite declarations
-    reg [10:0] char_x;
-    reg [9:0] char_y;
-    reg char_frame;
     sprite #(.WIDTH(20), .HEIGHT(10), .LOG_FRAMES(1)) character 
                        (.vclock(vclock), .hcount(hcount), .x(char_x), .vcount(vcount),
-                       .y(char_y), .curr_frame(char_frame), .p_rgb(character_rgb)
+                       .y(vpos), .curr_frame(char_frame), .p_rgb(character_rgb)
                        );
     
-    initial begin
+    //background declarations
+    background #(.ABOVE(0)) lower_background
+                (.vclock(vclock), .hcount(hcount), .vcount(vcount), .prof_hcount(wave[hcount[9:0]]), .p_rgb(l_bg_rgb));
+    
+    background #(.ABOVE(1)) upper_background
+                (.vclock(vclock), .hcount(hcount), .vcount(vcount), .prof_hcount(wave[hcount[9:0]]), .p_rgb(u_bg_rgb));
+    
+    initial begin //initial values
         char_x = 0;
-        char_y = 382;
+        vpos = 384;
         char_frame = 0;
     end
     
-    always @(posedge vclock) begin
+    //at each new frame
+    reg [10:0] i;
+    always @(negedge vsync) begin
+        //update values
+        for (i = 0; i < 1024; i = i+1) begin
+            wave[i] <= next_wave[i];
+        end
         offset <= p_offset;
         vpos <= p_vpos;
         
-        if(character_rgb) begin
-            p_rgb <= character_rgb;
+    end
+    
+    //at each pixel
+    always @(posedge vclock) begin
+        if (reset) begin //reset values
+            char_x <= 0;
+            char_frame <= 0;
+            p_rgb <= 0; //temporarily display black
         end
         else begin
-            p_rgb <= 12'b111100000000; //default display all pixels red
+            //shift in waveform data
+            if (wave_we) begin
+                next_wave[wave_index] <= wave_prof;
+            end
+            
+            
+            //if character data exists for this pixel
+            if(character_rgb) begin //use that
+                p_rgb <= character_rgb;
+            end
+            else if (l_bg_rgb) begin //otherwise use lower background data
+                p_rgb <= l_bg_rgb;
+            end
+            else if (u_bg_rgb) begin
+                p_rgb <= u_bg_rgb; //otherwise use upper background data
+            end
+            else begin //if no data exists for this pixel
+                p_rgb <= 12'hF00; //default display red
+            end
         end
+        
         
     end
     
@@ -74,7 +124,7 @@ module sprite #(parameter WIDTH=10,
         
         //assign new pixel value
         if ((hcount < x + WIDTH) & (hcount > x) & (vcount < y + HEIGHT) & (vcount > y)) begin //if within box
-            p_rgb <= 12'b111111111111; //for now, within the square is white
+            p_rgb <= 12'h0F0; //for now, within the square is green
         end
         else begin //otherwise
             p_rgb <= 12'b0; //elsewhere is empty.
@@ -103,7 +153,12 @@ module background #(parameter ABOVE=0, //1 if above dividing profile
         
         //assign new pixel value
         if (ABOVE ^ (vcount > prof_hcount)) begin //if in specified area
-            p_rgb <= 12'b111111111111; //for now, within the square is white
+            if (ABOVE) begin
+                p_rgb <= 12'hFFF; //for now, upper background is white
+            end
+            else begin
+                p_rgb <= 12'h00F; //for now, lower background is blue
+            end
         end
         else begin //otherwise
             p_rgb <= 12'b0; //elsewhere is empty.
--- nexys.v
+++ nexys.v
@@ -37,10 +37,22 @@ module nexys(
    output[7:0] AN    // Display 0-7
    );
     
-    // 65MHz clock generation from IP
+    //reset signal
+    wire power_on_reset;    // remain high for first 16 clocks
+    SRL16 reset_sr (.D(1'b0), .CLK(CLK100MHZ), .Q(power_on_reset),
+               .A0(1'b1), .A1(1'b1), .A2(1'b1), .A3(1'b1));
+    defparam reset_sr.INIT = 16'hFFFF;
+    
+    //user reset
+    wire reset,user_reset;
+    debounce center(.reset(power_on_reset),.clock(CLK100MHZ),.noisy(BTNC),.clean(user_reset));
+    assign reset = user_reset | power_on_reset;
+    
+    
+    
+    //65MHz clock generation from IP
     wire locked;
     wire clock_65mhz;
-    reg reset;
     clk_wiz_0 gen_65mhz(.clk_100mhz(CLK100MHZ), .clk_65mhz(clock_65mhz), .reset(reset), .locked(locked));
     
 // create 25mhz system clock
@@ -58,10 +70,10 @@ module nexys(
 //
 //  remove these lines and insert your lab here
 
-    assign LED = SW;     
+    //assign LED = SW;     
     assign JA[7:1] = 7'b0;
     assign JA[0] = clock_65mhz;
-    assign data = {28'h0123456, SW[3:0]};   // display 0123456 + SW
+    //assign data = {28'h0123456, SW[3:0]};   // display 0123456 + SW
     assign LED16_R = BTNL;                  // left button -> red led
     assign LED16_G = BTNC;                  // center button -> green led
     assign LED16_B = BTNR;                  // right button -> blue led
@@ -79,7 +91,11 @@ module nexys(
 
  
 //////////////////////////////////////////////////////////////////////////////////
-// sample Verilog to generate color bars
+// temporary organization / testing rig for peripheral modules
+    
+    //parameters only for later calculations; do not change
+    parameter SCREEN_HEIGHT = 768;
+    parameter SCREEN_WIDTH = 1024;
     
     //inputs and outputs
     wire [10:0] hcount; //vga
@@ -88,10 +104,10 @@ module nexys(
     
     wire [11:0] p_rgb; //current output pixel
     
-    wire [10:0] p_offset; //current player horizontal position (positive as wave moves left)
-    wire [9:0] p_vpos; //current player vertical position
-    wire [9:0] wave_prof[1023:0]; //current waveform profile
-    wire [9:0] prev_wave_prof[1023:0];
+    reg [10:0] p_offset; //current player horizontal position (positive as wave moves left)
+    reg [9:0] p_vpos; //current player vertical position
+    reg [9:0] wave_prof[1023:0]; //current waveform profile
+    reg [9:0] prev_wave_prof[1023:0];
     
     wire [10:0] frequency;
     assign frequency = 11'b0;
@@ -99,13 +115,72 @@ module nexys(
     assign new_f = 0;
     wire wave_ready;
     
-    reg [9:0] disp_wave;
-    reg wave_clk;
+    
+    wire [9:0] disp_wave;
+    reg wave_we;
+    reg [10:0] wave_index;
+    wire disp_sel; // if 0, display horizontal profile.  If 1, display ramp
+    reg prev_disp_sel; //previous value of disp_sel; 65mhz
+    reg prev_up, prev_down, prev_left, prev_right;
+    
+    //button outputs
+    wire up;
+    wire down;
+    wire left;
+    wire right;
+    
+    //assigning buttons
+    debounce sw0(.reset(reset),.clock(clock_65mhz),.noisy(SW[0]),.clean(disp_sel));
+    debounce dbu(.reset(reset),.clock(clock_65mhz),.noisy(BTNU),.clean(up));
+    debounce dbd(.reset(reset),.clock(clock_65mhz),.noisy(BTND),.clean(down));
+    debounce dbl(.reset(reset),.clock(clock_65mhz),.noisy(BTNL),.clean(left));
+    debounce dbr(.reset(reset),.clock(clock_65mhz),.noisy(BTNR),.clean(right));
     
     
-    //parameters only for later calculations; do not change
-    parameter SCREEN_HEIGHT = 768;
-    parameter SCREEN_WIDTH = 1024;
+    initial begin
+        p_vpos = 384;
+        wave_index = 0;
+    end
+    
+    assign disp_wave = disp_sel ? wave_index[9:0] : 10'd384;
+    
+    always @(posedge clock_65mhz) begin
+        prev_disp_sel <= disp_sel;
+        prev_up <= up;
+        prev_down <= down;
+        prev_left <= left;
+        prev_right <= right;
+        
+        
+        
+        if (prev_disp_sel != disp_sel) begin
+            wave_index <= 0;
+            wave_we <= 1;
+        end
+        else begin
+            if (wave_index < 1024) begin
+                wave_we <= 1;
+                wave_index <= wave_index + 1;
+                
+            end
+            else begin
+                wave_we <= 0;
+            end
+            
+            if ((up & !prev_up) & (p_vpos > 0)) begin
+                p_vpos <= p_vpos - 100;
+            end
+            else if ((down & !prev_down) & (p_vpos < SCREEN_HEIGHT-1)) begin
+                p_vpos <= p_vpos + 100;
+            end
+            else begin
+                p_vpos <= p_vpos;
+            end
+        end
+        
+    end
+    
+    
     
     xvga vga1(.vclock(clock_65mhz),.hcount(hcount),.vcount(vcount),
           .hsync(hsync),.vsync(vsync),.blank(blank));
@@ -113,9 +188,9 @@ module nexys(
     //wave_logic wave_logic(.reset(reset), .clock(clock_65mhz), .frequency(frequency), .new_f(new_f),
     //                      .wave_prof(wave_prof), .prev_wave_prof(prev_wave_prof), .wave_ready(wave_ready));
     
-    
-    display display(.reset(reset), .p_offset(p_offset), .p_vpos(p_vpos), .wave_prof(disp_wave), 
-                    .wave_clk(wave_clk), .vclock(clock_65mhz), .hcount(hcount), .vcount(vcount),
+    wire[9:0] vpos;
+    display display(.reset(reset), .p_offset(p_offset), .p_vpos(p_vpos), .wave_prof(disp_wave), .wave_index(wave_index),
+                    .wave_we(wave_we), .vclock(clock_65mhz), .hcount(hcount), .vcount(vcount),
                     .hsync(hsync), .vsync(vsync), .blank(blank), .p_rgb(p_rgb));
     
     
@@ -126,10 +201,12 @@ module nexys(
     assign VGA_HS = ~hsync;
     assign VGA_VS = ~vsync;
     
-    
-    initial begin
-        reset = 0;
-    end
+    //test outputs
+    //assign data[11:0] = {1'b0, reset_count}; //last three digits disp_wave
+    assign data[31:20] = {2'b0, p_vpos}; //first three digits wave_index
+    assign LED[0] = 1;
+    assign LED[1] = up;
+    assign LED[2] = down;
     
 endmodule
 
